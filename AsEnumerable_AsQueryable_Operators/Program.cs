1add131c6601d9b9c26e6e08ff72fde806b3faaf
Suraj Varade <suraj.varade@hotmail.com>
2020-11-20 16:53:41 +0530

    AsEnumerable/AsQueryable

---
+++ AsEnumerable_AsQueryable_Operators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsEnumerable_AsQueryable_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeContextDBClassDataContext employeeContext = new EmployeeContextDBClassDataContext();
+            //If i use AsEnumerable then my query(where/Orderby) will be execute at client side.
+            //filter logic will be executed at client side.
+            var result = employeeContext.Employees.AsEnumerable().Where(x => string.Equals(x.Gender, "Male")).OrderByDescending(x => x.Salary).Take(5);
+            foreach(Employee emp in result)
+            {
+                Console.WriteLine("ID : "+emp.ID+" , NAME : "+emp.Name+" , GENDER : "+emp.Gender+" , Salary : "+emp.Salary);
+            }
+
+            //filter logic will be executed at server side.
+            var result1 = employeeContext.Employees.AsQueryable().Where(x => string.Equals(x.Gender, "Male")).OrderByDescending(x => x.Salary).Take(5);
+            foreach (Employee emp in result1)
+            {
+                Console.WriteLine("ID : " + emp.ID + " , NAME : " + emp.Name + " , GENDER : " + emp.Gender + " , Salary : " + emp.Salary);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
