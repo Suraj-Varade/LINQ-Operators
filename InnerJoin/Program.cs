264e19d7bfff3a36a9a75aeea37245a681a30f8e
Suraj Varade <suraj.varade@hotmail.com>
2020-11-25 09:16:03 +0530

    Joins - GroupJoin (outerJoin) and Join (innerJoin)

---
+++ InnerJoin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- InnerJoin -----------");
+            //var result = Employee.GetAllEmployees().Join(Department.GetAllDepartments(),
+            //                        e => e.DepartmentID,
+            //                        d => d.ID,
+            //                        (employee, department) => new
+            //                        {
+            //                           EmployeeName = employee.Name,
+            //                           DepartmentName = department.Name
+            //                        });
+
+            Console.WriteLine("--------- Sql Like Syntax ------------");
+            var resultUsingSqlLikeSyntax = from e in Employee.GetAllEmployees()
+                                           join d in Department.GetAllDepartments() on
+                                           e.DepartmentID equals d.ID 
+                                           select new {
+                                               DepartmentName = d.Name,
+                                               EmployeeName = e.Name
+                                           };
+
+
+            foreach(var res in resultUsingSqlLikeSyntax)
+            {
+                Console.WriteLine(res.EmployeeName+"    "+res.DepartmentName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Join/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Group Join
+
+            var employeesByDepartment = Department.GetAllDepartments().GroupJoin(Employee.GetAllEmployees(), x => x.ID, y => y.DepartmentID,
+                                        (department, employees) => new
+                                        {
+                                            dept = department,
+                                            emp = employees
+                                        });
+
+            foreach(var department in employeesByDepartment)
+            {
+                Console.WriteLine(department.dept.Name + " - \n"+string.Join("\n",department.emp.Select(x=>x.Name)));
+                Console.WriteLine();
+            }
+
+            //using Sql like syntax....
+            var employeesByDepartmentUsingSqlLikeSyntax = from d in Department.GetAllDepartments()
+                                                          join e in Employee.GetAllEmployees()
+                                                          on d.ID equals e.DepartmentID into egroup
+                                                          select new
+                                                          {
+                                                              employees = egroup,
+                                                              department = d
+                                                          };
+
+            foreach (var department in employeesByDepartmentUsingSqlLikeSyntax)
+            {
+                Console.WriteLine(department.department.Name + " - \n" + string.Join("\n", department.employees.Select(x=>x.Name)));
+                
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
